2278ad944ab9a5a90d2407760ce98349c91a8344
OmegaCore <atherealprime@gmail.com>
2024-11-08 17:31:48 -0600

    More shenanigans - Working on the Broly Mech

--- src/data/weapons/onhit/AEG_GiganticPulseShot.java
+++ src/data/weapons/onhit/AEG_GiganticPulseShot.java
@@ -4,8 +4,6 @@ import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.CombatEntityAPI;
 import com.fs.starfarer.api.combat.DamagingProjectileAPI;
 import com.fs.starfarer.api.combat.OnHitEffectPlugin;
-
-
 import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI;
 import org.lazywizard.lazylib.MathUtils;
 import org.lwjgl.util.vector.Vector2f;
@@ -33,6 +31,11 @@ public class AEG_GiganticPulseShot implements OnHitEffectPlugin {
             float size = MathUtils.getRandomNumberInRange(MIN_SIZE, MAX_SIZE);
             Color color = COLORS[random.nextInt(COLORS.length)];
             Vector2f velocity = MathUtils.getPointOnCircumference(null, MathUtils.getRandomNumberInRange(50f, 200f), random.nextFloat() * 360f);
+
+            // Adjust velocity to create a splatter effect
+            velocity.scale(random.nextFloat() * 2f); // Randomize the speed
+            velocity.x += random.nextFloat() * 50f - 25f; // Add some randomness to the direction
+            velocity.y += random.nextFloat() * 50f - 25f;
 
             engine.addHitParticle(point, velocity, size, 1f, 1f, color);
         }
