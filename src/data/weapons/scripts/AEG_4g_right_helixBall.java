23937b38bf7988dcb601fc9e8ee9c932305f8601
OmegaCore <atherealprime@gmail.com>
2025-06-10 23:02:50 -0500

    Update to 9.8 Successful... Damn Arma 3.1 crashes so need to keep 3.0 (Not updated yet)  4G revamp! Goldion Crusher Check point - Still in progress

--- src/data/weapons/scripts/AEG_4g_right_helixBall.java
+++ src/data/weapons/scripts/AEG_4g_right_helixBall.java
@@ -1,5 +1,6 @@
 package data.weapons.scripts;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.input.InputEventAPI;
 import org.lazywizard.lazylib.MathUtils;
@@ -14,7 +15,7 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
 
     private final float SPEED = 350f;
     private final float MAX_DURATION = 10f;
-    private final float DAMAGE = 1000f;
+    private final float DAMAGE = 15000f;
     private final float EXPLOSION_RADIUS = 100f;
     private final int ORB_COUNT = 20;
 
@@ -98,69 +99,18 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
         // Update coreVelocity vector with new angle, maintain SPEED
         coreVelocity = MathUtils.getPoint(new Vector2f(), SPEED, newAngle);
     }
-
-    private void renderOrbs() {
-        Color gold = new Color(255, 230, 100, 220);
-        Color orange = new Color(255, 150, 50, 200);
-        Color red = new Color(255, 60, 60, 180);
-
-        for (int i = 0; i < ORB_COUNT; i++) {
-            float angle = (float) (Math.random() * Math.PI * 2f);
-            float radius = MathUtils.getRandomNumberInRange(40f, 150f);
-
-            Vector2f offset = MathUtils.getPoint(new Vector2f(), radius, angle);
-            Vector2f orbLocation = Vector2f.add(corePosition, offset, null);
-
-            // Velocity: either swirling or randomly bursting outward
-            Vector2f velocity = MathUtils.getRandomPointInCircle(null, MathUtils.getRandomNumberInRange(50f, 200f));
-            if (Math.random() < 0.3f) {
-                // Some are pulled inward a bit for contrast
-                velocity = Vector2f.sub(corePosition, orbLocation, null);
-                velocity.normalise();
-                velocity.scale(MathUtils.getRandomNumberInRange(40f, 80f));
-            }
-
-            float size = MathUtils.getRandomNumberInRange(30f, 60f);
-            float duration = MathUtils.getRandomNumberInRange(0.5f, 1.5f);
-
-            // Pick chaotic bright color
-            Color color = Math.random() < 0.4
-                    ? gold
-                    : (Math.random() < 0.7 ? orange : red);
-
-            engine.addNebulaParticle(
-                    orbLocation,
-                    velocity,
-                    size,
-                    2f,
-                    0.1f,
-                    0.2f,
-                    duration,
-                    color
-            );
-
-            if (Math.random() < 0.5f) {
-                // Occasional smoother particle for pop
-                engine.addSmoothParticle(
-                        orbLocation,
-                        velocity,
-                        size * 0.5f,
-                        1.5f,
-                        0.3f,
-                        new Color(255, 255, 180, 255)
-                );
-            }
-        }
-    }
-
     private void explode() {
         exploded = true;
+        Global.getSoundPlayer().playSound("hit_hull_heavy_energy", 1f, 1f, corePosition, coreVelocity);
         spawnExplosionFX();
         engine.removePlugin(this);
     }
 
     private void explode(CombatEntityAPI target) {
         exploded = true;
+        Global.getSoundPlayer().playSound("hit_shield_heavy_energy", 1f, 1f, corePosition, coreVelocity);
+        Global.getSoundPlayer().playSound("rifttorpedo_explosion", 0.8f, 1.1f, corePosition, coreVelocity);
+        Global.getSoundPlayer().playSound("riftcascade_rift", 0.6f, 1.2f, corePosition, coreVelocity);
         spawnExplosionFX();
         engine.applyDamage(
                 target,
@@ -199,18 +149,18 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
         engine.addSmoothParticle(
                 corePosition,
                 new Vector2f(),
-                80f,
+                80f + MathUtils.getRandom().nextInt(80),
                 2.5f,
                 0.2f,
-                new Color(255, 245, 180, 255)
+                new Color(255, 245 - MathUtils.getRandom().nextInt(80), 180 - MathUtils.getRandom().nextInt(50), 255 - MathUtils.getRandom().nextInt(25))
         );
         engine.addSmoothParticle(
                 corePosition,
                 new Vector2f(),
-                120f,
+                120f + MathUtils.getRandom().nextInt(80),
                 1.8f,
                 0.1f,
-                new Color(255, 200, 50, 200)
+                new Color(255, 200 - MathUtils.getRandom().nextInt(80),MathUtils.getRandom().nextInt(50), 200 - MathUtils.getRandom().nextInt(50))
         );
     }
     private void renderOrbitingPlasma() {
@@ -246,7 +196,7 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
                     MathUtils.getRandomNumberInRange(20f, 40f),
                     1.2f,
                     0.2f,
-                    new Color(255, 200 + MathUtils.getRandom().nextInt(50), 50 + MathUtils.getRandom().nextInt(50), 220)
+                    new Color(255, 200 + MathUtils.getRandom().nextInt(50), 50 + MathUtils.getRandom().nextInt(50), 220 - MathUtils.getRandom().nextInt(70))
             );
         }
     }
@@ -265,7 +215,7 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
                 0.2f,
                 0.3f,
                 1.5f,
-                new Color(255, 240, 120, 140)
+                new Color(255, 240 - MathUtils.getRandom().nextInt(70), 120 - MathUtils.getRandom().nextInt(80), 140 + MathUtils.getRandom().nextInt(80))
         );
     }
     private CombatEntityAPI getNearestEnemy(CombatEngineAPI engine, ShipAPI source, Vector2f loc, float range) {
@@ -282,42 +232,6 @@ public class AEG_4g_right_helixBall implements EveryFrameCombatPlugin {
         }
 
         return nearest;
-    }
-    private void renderChargeUpFX() {
-        Color gold = new Color(255, 230, 100, 200);
-        Color orange = new Color(255, 120, 40, 180);
-        Color red = new Color(255, 60, 60, 160);
-
-        for (int i = 0; i < 10; i++) {
-            float angle = (float) (Math.random() * Math.PI * 2f);
-            float baseRadius = MathUtils.getRandomNumberInRange(60f, 90f);
-            boolean isStray = Math.random() < 0.2; // 20% stray particles
-
-            float radius = isStray ? MathUtils.getRandomNumberInRange(100f, 160f) : baseRadius;
-
-            Vector2f spawnPoint = MathUtils.getPoint(corePosition, radius, angle);
-            Vector2f toCore = Vector2f.sub(corePosition, spawnPoint, null);
-            toCore.normalise();
-            toCore.scale(MathUtils.getRandomNumberInRange(30f, 60f)); // speed towards center
-
-            float size = MathUtils.getRandomNumberInRange(6f, 15f);
-            float duration = isStray ? 2.5f : 1.2f;
-
-            Color color = Math.random() < 0.5
-                    ? gold
-                    : (Math.random() < 0.7 ? orange : red);
-
-            engine.addNebulaParticle(
-                    spawnPoint,
-                    toCore,
-                    size,
-                    1.8f,
-                    0.1f,
-                    0.2f,
-                    duration,
-                    new Color(color.getRed(), color.getGreen(), color.getBlue(), 200)
-            );
-        }
     }
 
 
